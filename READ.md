81e1c9524359eac894305e1f80a3f755f353f61b
unknown <thakaliishneha_31@icloud.com>
2024-05-08 07:56:06 +0545

    project initialized

---
+++ READ.md
@@ -0,0 +1,4 @@
+## Practice Project
+This is the best project in the world to learn git
+
+Author: Dipak Niroula
---
+++ main.py
@@ -0,0 +1,12 @@
+import backend
+
+def main():
+    user_name=input("enter name:")
+    greet= backend.hello_world(user_name)
+    print (greet)
+
+
+
+main()
+
+    
